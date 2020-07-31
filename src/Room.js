bc64e064d769bf39baf267ac839222fa823d97a1
alex-hladun <alexhladun@gmail.com>
2020-07-31 00:20:23 +0000

    Added Avery's code

--- src/Room.js
+++ src/Room.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from 'react';
 import Video from 'twilio-video';
 import Participant from './Participant';
 
+const startTime = 5;
+
 const Room = ({ roomName, token, handleLogout }) => {
   const [room, setRoom] = useState(null);
+  console.log("Room -> room", room)
   const [participants, setParticipants] = useState([]);
+  // console.log("Room -> participants", participants)
+  const [time, setTime] = useState(startTime);
+  const [active, setActive] = useState(false)
+  const [turn, setTurn] = useState(0)
 
   useEffect(() => {
     const participantConnected = participant => {
@@ -45,9 +52,78 @@ const Room = ({ roomName, token, handleLogout }) => {
     <Participant key={participant.sid} participant={participant} />
   ));
 
+  const toggleMute = function () {
+    // console.log(room.localParticipant);
+    room.localParticipant.audioTracks.forEach(publication => {
+      if (publication.track.isEnabled) {
+        //set
+        // console.log('muted');
+        publication.track.disable();
+      } else {
+        // console.log('unmuted');
+        publication.track.enable();
+      }
+    });
+  }
+  
+  function handleTrackDisabled(track) {
+    console.log('track -->', track);
+    track.on('disabled', () => {
+      return console.log('Remote Person Muted');
+      // return true;
+    });
+    return console.log('Remote Person Unmuted');
+    // return false;
+  }
+ 
+    if(room){
+      room.participants.forEach(participant => {
+      // console.log("Room -> participant", participant)
+        participant.tracks.forEach(publication => {
+        // console.log("Room -> publication", publication.isSubscribed)
+          if (publication.isSubscribed) {
+            // console.log("Room -> publication.track", publication.track)
+            handleTrackDisabled(publication.track);
+          }
+          publication.on('subscribed', handleTrackDisabled);
+        });
+      });
+    }
+
+  const toggle = function () {
+    setActive(!active);
+  }
+
+  useEffect(()=>{
+   let timer = null;
+   if(turn < 2){
+    if (active && time >= 0) {
+      timer = setTimeout(() => {
+        setTime(time - 1)
+      }, 1000);
+    } else if (active && time < 0) {
+      setTurn(prevTurn => prevTurn + 1)
+      setTime(startTime);
+    } else if (!active) {
+      clearTimeout(timer)
+    }
+   } else {
+    clearTimeout(timer)
+   }
+   
+
+  },[active, time, turn]);
+    
+ 
+
   return (
     <div className="room">
       <h2>Room: {roomName}</h2>
+      <h3>{time}</h3>
+      <h3>{turn}</h3>
+      <h3>{(turn === 2 && "Debate over")}</h3>
+      <h3 onClick={toggle}>On/Off</h3>
+      {/* <h3>{message}</h3> */}
       <button onClick={handleLogout}>Log out</button>
       <div className="local-participant">
         {room ? (
@@ -59,6 +135,7 @@ const Room = ({ roomName, token, handleLogout }) => {
           ''
         )}
       </div>
+      <div onClick={() => toggleMute()}>mute</div>
       <h3>Remote Participants</h3>
       <div className="remote-participants">{remoteParticipants}</div>
     </div>
