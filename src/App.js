eb8b76b9e0518352537c5f6a6e4b4f5c48ab0002
Trevor Thomas <thomastrevor1@gmail.com>
2020-07-31 10:45:41 -0600

    Added lobby and lobby item component

--- src/App.js
+++ src/App.js
@@ -2,32 +2,29 @@ import React from 'react';
 import './App.css';
 import VideoChat from './VideoChat';
 import ReactDOM from 'react-dom';
-import Rating from './components/partials/rating'
-import DiscreteSlider from './components/partials/slider'
+// import Rating from './components/partials/rating'
+// import DiscreteSlider from './components/partials/slider'
+        
+import Lobby from './components/lobby/lobby'
+
 import './components/partials/slider.css'
 
 const App = () => {
   return (
     <div className="app">
       <header>
-        <h1>Video Chat with Hooks</h1>
+        <h1 style={{backgroundColor: "black"}}>Debatr</h1>
       </header>
       <main>
-        <VideoChat />
+      <VideoChat />
       </main>
-      <div>
-        <Rating />
-      </div>
-      <div>
-        <DiscreteSlider />
-      </div>
       <footer>
         <p>
           Made with{' '}
           <span role="img" aria-label="React">
             ⚛️
           </span>{' '}
-          by <a href="">AAA+</a>
+          by <p>AAA+</p>
         </p>
       </footer>
     </div>
