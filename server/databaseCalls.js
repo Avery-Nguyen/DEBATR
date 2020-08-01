c756158e201b4821dade55e560466c88c2c66dd4
Alex Hladun <57810269+alex-hladun@users.noreply.github.com>
2020-08-01 12:11:24 -0600

    Add db calls

--- server/databaseCalls.js
+++ server/databaseCalls.js
@@ -1,12 +1,16 @@
 const db = require('./db.js')
 
+
 const postResultsToDatabase = () => {
   return db.query(`
   insert into room_logs (topic_id, host_id, contender_id, agreement_rating) 
   VALUES ($1, $2, $3, $4);
-    `, [this.topic, host_id, contender_id, this.agreement_rating])
-      .then(res => {
-        // console.log('Response from SQL', res);
-        return res.rows;
-      });
+  `, [this.topic, host_id, contender_id, this.agreement_rating])
+    .then(res => {
+      // console.log('Response from SQL', res);
+      return res.rows;
+    });
+}
+export default {
+  postResultsToDatabase
 }
