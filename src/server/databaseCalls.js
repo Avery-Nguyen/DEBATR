d47f800042dcd60ed533d12dafae444ec2dc9d72
Alex Hladun <57810269+alex-hladun@users.noreply.github.com>
2020-08-02 11:53:03 -0600

    Moved /server folder to /src/

---
+++ src/server/databaseCalls.js
@@ -0,0 +1,90 @@
+const client = require('./db.js');
+
+const getRoomRecords = (limit = 10) => {
+  return client.query(`SELECT * FROM room_logs
+  JOIN topics ON room_logs.topic_id = topics.id
+  ORDER BY room_logs.date_time DESC
+  LIMIT $1;
+  `, [limit])
+  .then(res => {
+    return res.rows
+  })
+}
+
+const postResultsToDatabase = (data) => {
+  // Get topic ID from room state?
+
+  // Query to get host_id and contender id?
+  return client.query(`
+  INSERT INTO room_logs (topic_id, host_id, contender_id) 
+  VALUES ($1, $2, $3);
+  `, [data.topic_id, data.host_id, data.contender_id])
+    .then(res => {
+      // console.log('Response from SQL', res);
+      return res.rows;
+    });
+}
+
+const postUserRating = (data) => {
+  return client.query(`
+  INSERT INTO ratings (from_user_id, to_user_id, rating, points) 
+  VALUES ($1, $2, $3);
+  `, [data.from_user_id, data.to_user_id, data.rating, data.points])
+    .then(res => {
+      // console.log('Response from SQL', res);
+      return res.rows;
+    });
+}
+
+const postAgreementRating = (data) => {
+  return client.query(`
+  INSERT INTO agreement_ratings (room_log_id, user_id, agreement_rating) 
+  VALUES ($1, $2, $3);
+  `, [data.room_log_id, data.user_id, data.agreement_rating])
+    .then(res => {
+      // console.log('Response from SQL', res);
+      return res.rows;
+    });
+}
+
+const getUserInfoByEmail = (email) => {
+  return client.query(`SELECT * FROM users
+  WHERE email = $1
+  `, [email])
+  .then(res => {
+    console.log(res.rows)
+    return res.rows
+  })
+}
+const checkEmailTaken = (email) => {
+  client.query(`SELECT * FROM users
+  WHERE email = $1
+  `, [email])
+  .then(res => {
+    if (res.rows) {
+      return true
+    } else {
+      return false
+    }
+  })
+
+}
+const createUser = (email, first_name, last_name, username, password, avatar_url) => {
+  return client.query(`
+  INSERT INTO users (first_name, last_name, email, username, password, avatar_url)
+  VALUES ($1, $2, $3, $4, $5, $6);
+  `, [email, first_name, last_name, username, password, avatar_url])
+  .then(res => {
+    return res.rows
+  })
+}
+
+export default {
+  postResultsToDatabase, 
+  getRoomRecords, 
+  postUserRating, 
+  postAgreementRating, 
+  getUserInfoByEmail, 
+  checkEmailTaken, 
+  createUser
+}
---
+++ src/server/routes.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const router = express.Router()
+const { 
+  postResultsToDatabase,
+  getRoomRecords, 
+  postUserRating, 
+  postAgreementRating, 
+  getUserInfoByEmail, 
+  checkEmailTaken, 
+  createUser } = require('./databaseCalls.js');
+const bcrypt = require('bcrypt');
+
+
+//axios request
+// - /api/users/ POST
+//   - Create an account
+// - /api/users/ GET
+//   - Retrieve account details for logging in.
+// - /api/users/reviews GET
+//   - Get user reviews
+// - /api/games GET
+//   - Retrieve game results
+
+
+router.get('/rooms', function(req, res) {
+  getRoomRecords()
+    .then(sqlResponse => {
+      res.send(sqlResponse)
+    })
+})
+
+router.post('/users/ratings', function(req, res) {
+  const from_user_id = req.body.from_user_id
+  const to_user_id = req.body.to_user_id
+  const rating = req.body.rating
+  const points = req.body.points
+
+  postUserRating({
+    from_user_id,
+    to_user_id,
+    rating,
+    points
+  }).then(res => {
+    return res.rows
+  })
+})
+
+router.post('/agreement_ratings', function(req, res) {
+  const room_log_id = req.body.room_log_id
+  const user_id = req.body.user_id
+  const agreement_rating = req.body.agreement_rating
+
+  postAgreementRating({
+    room_log_id,
+    user_id,
+    agreement_rating,
+  }).then(res => {
+    return res.rows
+  })
+})
+
+router.get('/login', function (req,res) {
+  const loginInfo = req.body;
+  const userID = getUserInfoByEmail(loginInfo.email);
+
+  if (!userID) {
+    // TODO: Add a 'user does not exist error'
+    return res.redirect('login/401');
+  }
+  
+  bcrypt.compare(loginInfo.password, userID.password, function(err, result) {
+    if (result) {
+      req.session.userID = userID;
+      return res.redirect('/urls');
+    } else {
+    // TODO: Add a incorrect login page or something
+      return res.redirect('login/401');
+    }
+  });
+})
+
+router.post('/users', function(req, res) {
+  if (req.body.email === "" || req.body.password === "") {
+    // TODO: Change this
+    return res.redirect('/register/empty');
+  }
+
+  if (checkEmailTaken(req.body.email)) {
+    // TODO: Change this
+    return res.redirect('/register/taken');
+  }
+
+  createUser(req.body.email, req.body.first_name, req.body.last_name, req.body.username, req.body.password, req.body.avatar_url)
+  .then((sqlResponse) => {
+    console.log(sqlResponse.rows)
+    // Need to assign cookie
+    // req.session.userID = sqlResponse.rows;
+
+    res.send(sqlResponse)
+  })
+})
+
+
