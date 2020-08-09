7764991a99674fafc6a9e3b653463505c29773ab
Alex Hladun <57810269+alex-hladun@users.noreply.github.com>
2020-08-09 10:03:51 -0600

    Improved git login button

--- src/components/sign-in/signIn.jsx
+++ src/components/sign-in/signIn.jsx
@@ -55,9 +55,9 @@ export default function SignIn(props) {
 
     if (email === '') {
       dispatch({ type: 'SET_LOGIN_ERROR', payload: { error: 'Email cannot be blank' } })
-    // Hidden to log in as a@a 
+      // Hidden to log in as a@a 
       // } else if (!/\S+@\S+\.\S+/.test(email)) {
-    //   dispatch({ type: 'SET_LOGIN_ERROR', payload: { error: 'Email address is invalid' } })
+      //   dispatch({ type: 'SET_LOGIN_ERROR', payload: { error: 'Email address is invalid' } })
     } else if (password === '') {
       dispatch({ type: 'SET_LOGIN_ERROR', payload: { error: 'Password cannot be blank' } })
     } else {
@@ -71,7 +71,7 @@ export default function SignIn(props) {
         .then((res) => {
           if (res.data.authenticated) {
             dispatch({ type: 'SET_LOGIN_ERROR', payload: { error: null } })
-            
+
             dispatch({ type: 'SET_USER_AVATAR_URL', payload: res.data.userAvatarURL })
             dispatch({ type: 'SET_USERNAME', payload: res.data.username })
             dispatch({ type: 'SET_USER_ID', payload: res.data.userID })
@@ -111,10 +111,37 @@ export default function SignIn(props) {
           Sign in
         </Typography>
 
-        <a href="http://localhost:3001/api/login/github" id="github-button" class="btn btn-block btn-social btn-github">
-          <i class="fa fa-github"></i> Sign in with Github
-        </a>
-
+<div style={{display:'flex',justifyContent:'center', width:'80%'}}>
+        <form action="http://localhost:3001/api/login/github" style={{width:'90%'}}>
+          <button
+            style={{
+              display: 'block',
+              border: '0px',
+              borderRadius: '3px',
+              boxShadow: 'rgba(0, 0, 0, 0.5) 0px 1px 2px',
+              color: 'rgb(255, 255, 255)',
+              cursor: 'pointer',
+              fontSize: '19px',
+              margin: '5px',
+              width: 'calc(90% - 10px)',
+              overflow: 'hidden',
+              padding: '0px 10px',
+              userSelect: 'none',
+              height: '50px',
+              background: 'rgb(51, 51, 51)'
+            }}
+            type="submit">
+            <div style={{ "alignItems": "center", "display": "flex", "height": "100%" }}>
+              <div style={{ "display": "flex", "justifyContent": "center", "minWidth": "26px" }}>
+                <svg fill="#FFFFFF" role="img" viewBox="0 0 24 24" width="26px" height="26px" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path></svg>
+              </div>
+              <div style={{ width: '10px' }}></div>
+              <div style={{ textAlign: "left", width: "100%" }}>Login with GitHub</div>
+            </div>
+          </button>
+        </form>
+        </div>
 
         <form className={classes.form} onSubmit={submitLogin} noValidate>
           <TextField
